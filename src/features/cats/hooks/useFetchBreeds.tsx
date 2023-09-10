f1236869fe333a77bd9cde9ed7c540285d744c94
Maharlika Alberca <maharlikaalberca@gmail.com>
2023-09-11 00:12:05 +0800

    feat: add fetch breeds

---
+++ src/features/cats/hooks/useFetchBreeds.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { fetchBreeds } from '../api';
+
+export const useFetchBreeds = () => {
+  const fetchCatBreeds = () => {
+    try {
+      return fetchBreeds();
+    } catch (error) {
+      console.log({ error });
+    }
+  };
+
+  return {
+    fetchCatBreeds,
+  };
+};
