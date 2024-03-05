d85c6379f60d7f2208a1245b029038bee339b299
Maharlika Alberca <maharlikaalberca@gmail.com>
2024-03-05 16:13:29 +0800

    Setup as MFE

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import federation from '@originjs/vite-plugin-federation';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    federation({
+      name: 'remote-app',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './Cats': './src/features/cats/components/CatItem.tsx',
+      },
+      shared: ['react'],
+    }),
+    react(),
+    tsconfigPaths(),
+  ],
+  build: {
+    target: 'esnext',
+  },
 });
